badc32f08a42f4afccf019adcca46f989153acec
Tom W <tomw@tommys.tech>
2017-10-30 19:59:30 +0000

    added command line options to laserDriver.py and error checking

--- laserDriver.py
+++ laserDriver.py
@@ -1,5 +1,10 @@
+import argparse
+import importlib
 import time
 import serial
+import sys
+
+import laserinputs
 
 class LaserDisplayController(object):
     """
@@ -27,26 +32,79 @@ class LaserDisplayController(object):
         if len(displaytextformatted) > displaytextmaxlen:
             displaytextformatted = displaytextformatted[:displaytextmaxlen]
         modes = {'X':'off','A':'named animation','S':'static', 'M':'marquee', 'F':'flash'}
-        animations = ['plane','bicycle','building']
+        animations = ['PLANE','BIKE','BUILDING']
+        if displaymode not in modes.keys():
+            return 'invalid display mode selected'
+        if displaymode == 'A' and displaytext not in animations:
+            return 'invalid animation selected'
         if len(displaysize) < 2:
             displaysizeformatted = '0' + displaysize
         else:
             displaysizeformatted = displaysize
-        
         self.command = displaymode + displaysizeformatted + displaytextformatted + '\n'
+        return 'command format OK'
 
     def send_command(self):
         """
         send the command down the serial interface
         """
-        time.sleep(1)
-        self.seriallink.write(self.command.encode())
+        try:
+            time.sleep(1)
+            self.seriallink.write(self.command.encode())
+            return 'sent OK'
+        except Exception as error:
+            return 'ERROR unable to send the command - ' + str(error)
 
     def read_response(self):
         """
         read response from the serial port
         """
-        if self.seriallink.in_waiting > 0 :
+        try:
             return self.seriallink.readline().decode() 
-        else :
-            return ""
+        except Exception as error:
+            return 'ERROR unable to read from the serial interface - ' + str(error)
+
+
+def run_custom_display_script(scriptname):
+    """
+    provide a name of a script in the inputs folder excluding the .py extension
+    this function will import the module and run the scripts performactions function
+    it will return the results of that function as a string
+    """
+    mod = importlib.import_module('laserinputs.' + scriptname)
+    resultstring = mod.performactions()
+    return resultstring
+
+def main():
+    """
+    command line interface to the laser controller.
+    run this if script is run directly, i.e NOT IMPORTED
+    """
+    parser = argparse.ArgumentParser(description='command line app to send commands to the laser projector')
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument('-b', dest='baudrate', help='set baudrate, if not specified default is 9600 baud', type=int, default=9600)
+    parser.add_argument('-d', dest='serialdevice', help='specify serial device, default is /dev/ttyACM1', default='/dev/ttyACM1')
+    parser.add_argument('-s', dest='displaysize', help='size of the display', default='5')
+    parser.add_argument('-a', dest='animation', help='animation to select', default='S')
+    group.add_argument('-i', dest='input', help='custom input, must be the name of a script in the inputs folder')
+    group.add_argument('-t', dest='displaytext', help='text to display')
+    args = parser.parse_args()
+
+    if args.input:
+        lasertext = run_custom_display_script(args.input)
+    elif args.displaytext:
+        lasertext = args.displaytext
+    else:
+        parser.print_help()
+        sys.exit(1)
+    lc = LaserDisplayController(args.serialdevice, args.baudrate)
+    #response = lc.read_response()
+    #print(response)
+    lc.format_command(args.animation, args.displaysize, lasertext)
+    lc.send_command()
+    while 1:
+        response = lc.read_response()
+        print(response)
+
+if __name__ == '__main__':
+    main()
