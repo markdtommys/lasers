d03a7fa8c808cc95342139d386a6d28dbf501ba9
Tom W <tomw@tommys.tech>
2017-11-08 18:42:29 +0000

    made the output string shorter for currentweather.py

--- laserinputs/currentweather.py
+++ laserinputs/currentweather.py
@@ -1,8 +1,18 @@
 import feedparser
 
+def shorten_text(longstring):
+    """
+    take out uneeded words
+    """
+    wordstoremove = ['Temperature', 'Wind Direction', 'Wind Speed', 'Humidity', 'Pressure', 'Visibility', ':', ',']
+    for word in wordstoremove:
+        longstring = longstring.replace(word, '')
+    return longstring
+
 def performactions():
     feed = feedparser.parse('http://open.live.bbc.co.uk/weather/feeds/en/2653261/observations.rss')
     observation = feed['entries'][0]['summary_detail']['value']
     source = feed['feed']['title']
-    wxstring = source + ' ' + observation
+    wxsummary = shorten_text(observation)
+    wxstring = 'BBC Weather' + ' ' + wxsummary
     return wxstring
